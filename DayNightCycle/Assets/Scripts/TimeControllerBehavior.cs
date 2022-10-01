15b7738638f25224307d48f3adef7ca1c67aedac
KatelynWest <katelynwest225@gmail.com>
2022-10-01 16:57:04 -0500

    Created the project files

---
+++ DayNightCycle/Assets/Scripts/TimeControllerBehavior.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeControllerBehavior : MonoBehaviour
+{
+    private float _hoursInDay = 24.0f;
+    private float _minutesInHour = 60.0f;
+    private float _currHour = 0;
+    private float _currMinute = 0;
+
+    
+}
